0ca20187affdc7ccd56f1cd54e446c76b29b89e0
Mohamed Abdulla Covering <mode_vv@live.com>
2025-10-21 10:41:40 +0500

    ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,14 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import sanity from '@sanity/astro';
-import { sanityConfig } from './src/utils/sanity-client';
+import react from '@astrojs/react';
+// import tailwindcss from '@tailwindcss/vite';
+// import sanity from '@sanity/astro';
+// import { sanityConfig } from './src/utils/sanity-client';
 
 // https://astro.build/config
 export default defineConfig({
-    image: {
-        domains: ['cdn.sanity.io']
-    },
-    integrations: [sanity(sanityConfig)],
-    vite: {
-        plugins: [tailwindcss()],
-        server: {
-            hmr: { path: '/vite-hmr/' },
-            allowedHosts: ['.netlify.app']
-        }
-    },
+    integrations: [
+        react()
+    ],
     server: {
         port: 3000
     }
